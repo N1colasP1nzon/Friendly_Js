dcba4b43bb882ee0f008d6626b28db61df18568e
NicoPinzon <estudiopinzon@gmail.com>
2021-11-15 21:59:55 -0300

    Comenzando con Js

---
+++ script.js
@@ -0,0 +1,21 @@
+/* var nombre ="Nicolas"
+var edad = 25
+var esProfesor = true
+var nulo = null
+var indefinido =undefined
+
+let nombre1 = "Neyder"
+let gato = "Animal"
+
+const IVA = 1.21
+
+console.log(nombre)
+console.log(edad)
+console.log(esProfesor)
+console.log(IVA * 100)
+ */
+
+let numero1 = parseFloat( prompt("Igrese un numero: "))
+let numero2 = parseFloat( prompt("Ingrese un segundo numero: "))
+alert(numero1+numero2)
+
