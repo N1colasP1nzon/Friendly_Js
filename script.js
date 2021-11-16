a281683c9e4f82a7ad98bf284bccfa1b6c2dd3f4
NicoPinzon <estudiopinzon@gmail.com>
2021-11-15 22:18:40 -0300

    Se agrego para visualizar por consola

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-/* var nombre ="Nicolas"
-var edad = 25
-var esProfesor = true
-var nulo = null
-var indefinido =undefined
-
-let nombre1 = "Neyder"
-let gato = "Animal"
-
-const IVA = 1.21
-
-console.log(nombre)
-console.log(edad)
-console.log(esProfesor)
-console.log(IVA * 100)
- */
-
 let numero1 = parseFloat( prompt("Igrese un numero: "))
 let numero2 = parseFloat( prompt("Ingrese un segundo numero: "))
 alert(numero1+numero2)
+
+let nombre = prompt("Ingrese su nombre: ")
+let apellido = prompt("Ingrese su apellido")
+alert(`${nombre} ${apellido}`)
+
+let nombre1 = "Hola Nicolas Pinzon"
+console.log(nombre1)
+
+let numero3 = parseInt( prompt("Igrese un numero: "))
+let numero4 = parseInt( prompt("Ingrese un segundo numero: "))
+console.log(numero3+*numero4)
 
