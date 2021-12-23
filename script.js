b0f9824314b0c103f480d2d50c4da50e1512baa7
NicoPinzon <estudiopinzon@gmail.com>
2021-12-22 23:03:47 -0300

    Segunda entrega del proyecto final

--- script.js
+++ script.js
@@ -1,3 +1,119 @@
+class Cliente {
+    constructor(nombre, apellido, dni, email,) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+    
+    }
+
+   /*  incrementarSueldo(dineroAIncrementar) {
+        this.sueldo += dineroAIncrementar;
+    }
+
+    decrementarSueldo(dineroADecrementar) {
+        this.sueldo -= dineroADecrementar;
+    } */
+}
+
+let clientes;
+let formCliente = document.getElementById('formCliente')
+let divClientes = document.getElementById('divClientes')
+let parrafoError = document.getElementById('parrafoError')
+
+
+if(localStorage.getItem('clientes')) {
+    clientes = JSON.parse(localStorage.getItem('clientes'))
+} else {
+    clientes = []
+}
+
+//Obtengo informacion del formulario 
+formCliente.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let nombreCliente = document.getElementById("nombre").value
+    let apellidoCliente = document.getElementById("apellido").value
+    let dniCliente = document.getElementById("dni").value
+    let emailCliente = document.getElementById("email").value
+   /*  let sueldoCliente = document.getElementById("sueldo").value */
+
+    let objetoCliente = new Cliente(nombreCliente, apellidoCliente, dniCliente, emailCliente, /* sueldoCliente */)
+    
+    clientes.push(objetoCliente)
+
+    localStorage.setItem('clientes', JSON.stringify(clientes))
+
+    formCliente.reset()
+})
+
+//Muestro informacion del storage a traves de un boton
+document.getElementById('botonClientes').addEventListener('click', () => {
+    let clientesStorage = JSON.parse(localStorage.getItem('clientes'))
+
+    if(divClientes.children.length == 0) {
+        clientesStorage.forEach((clienteArray, indice) => {
+            divClientes.innerHTML += `
+                <div class="card" id="cliente${indice}" style="width: 18rem;margin: 6px">
+                    <div class="card-body">
+                        <h5 class="card-title">${clienteArray.nombre} ${clienteArray.apellido}</h5>
+                        <p class="card-text">${clienteArray.dni}</p>
+                        <p class="card-text">${clienteArray.email}</p>
+                        
+                        <button id="boton${indice}" class="btn btn-danger">Eliminar</button>
+                    </div>
+                </div>
+            
+            `
+        }) 
+
+        //Añado el evento eliminar a las cards
+        clientesStorage.forEach((clienteArray, indice) => {
+            document.getElementById(`boton${indice}`).addEventListener('click', () => {
+                document.getElementById(`cliente${indice}`).remove()
+                clientes.splice(indice, 1)
+                localStorage.setItem('clientes', JSON.stringify(clientes))
+                console.log(`El cliente ${clienteArray.nombre} ha sido eliminado`)
+            })
+        })
+
+    } else {
+        parrafoError.innerText = "Por favor deje de dar click a el boton Suscribirse como cliente"
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* let boton = document.getElementById("boton1")
 let botonInput = document.getElementById("botonInput")
 let inputColor = document.getElementById("inputColor")
@@ -16,7 +132,7 @@ inputColor.addEventListener('change ', (e) => {
 inputTexto.addEventListener('input', (e) => {
     console.log(e)
 }) */
-class Producto {
+/* class Producto {
     constructor(modelo, marca, precio) {
         this.modelo = modelo;
         this.marca = marca;
@@ -27,7 +143,7 @@ class Producto {
 
 const auto1 = new Producto("Megan", "Renault", 120000)
 const auto2 = new Producto("Logan", "Chevrolet", 20000)
-const auto3 = new Producto("Hylux", "Toyota", 5000000)
+const auto3 = new Producto("Hilux", "Toyota", 5000000)
 
 let autos = [auto1, auto2, auto3]
 let divAutos = document.getElementById("divAutos")
@@ -45,46 +161,11 @@ botonProducotos.addEventListener('click', () => {
             </div>
         </div>
         
-        `
+        ` */
         /*         console.log(autoEnArray)
                 console.log(indice) */
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*     })
+}) */
 
 /* class Registro {
     constructor(correo, contraseña) {
